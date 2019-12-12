96f8e8cb86977cbcbc5ca850a169643a47ce6a9b
Xenia <52275879+xenia19@users.noreply.github.com>
2019-12-12 20:13:00 +0500

    Added contact form

---
+++ common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Thank you for your message!");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
