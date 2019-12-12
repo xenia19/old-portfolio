32b463e9631337de3780c2970da2f059bb576260
Xenia <52275879+xenia19@users.noreply.github.com>
2019-12-12 20:16:08 +0500

    Update common.js

--- common.js
+++ common.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$("#form").submit(function() {
 		$.ajax({
 			type: "POST",
-			url: "/mail.php",
+			url: "portfolio/mail.php",
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
